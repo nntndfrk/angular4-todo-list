2b9449bb55c2e7eca2c9ee27ed16eaaa8e89e30a
nntndfrk <nntndfrk@gmail.com>
2017-11-01 18:34:23 +0200

    todo-app commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Model, TodoItem } from './model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-todo',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  model = new Model();
+  taskInput = '';
+
+  getName() {
+    return this.model.user;
+  }
+
+  getTodoItems() {
+    return this.model.items.filter(item => !item.done);
+  }
+
+  addItem(newItem) {
+    if (newItem !== '') {
+      this.model.items.push(new TodoItem(newItem, false));
+      this.taskInput = '';
+    }
+  }
 }
